fd3666f2b5e59aaffecd3bff74e57f118f805a2a
Ahmarul Abdi <rndmjck@gmail.com>
2024-10-26 22:01:14 +0700

    refactor: put code into spesific func.

--- main.go
+++ main.go
@@ -5,37 +5,62 @@ import (
 	"apitodocs/laravel"
 	"apitodocs/postman"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/joho/godotenv"
 )
 
-const LARAVEL = 1
+// currently support laravel 8 only
+const PHP73_LARAVEL8 string = "php73-laravel8"
+const BASE_URL = "http://localhost:8080"
+const PROTOCOL = "http"
+
+var fromFlag *string
+var baseUrlFlag *string
 
 func main() {
+	loadEnv()
+	flagParserAndModifier()
+
+	postmanCollection := collectionFrom()
+
+	exportToFile(postmanCollection)
+}
+
+func loadEnv() {
 	err := godotenv.Load(helpers.GetCurrentPath() + ".env")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
+}
 
-	from := 1
+func flagParserAndModifier() {
+	baseUrlFlag = flag.String("base-url", BASE_URL, "Custom base url")
+	fromFlag = flag.String("from", PHP73_LARAVEL8, "Laravel version")
+	flag.Parse()
+	flagModifier()
+	fmt.Println(*baseUrlFlag)
+}
 
+func collectionFrom() postman.Collection {
 	postmanCollection := postman.Collection{}
-	switch from {
-	case LARAVEL:
+	switch *fromFlag {
+	case PHP73_LARAVEL8:
 		postmanCollection = laravel.MakeCollection()
 	}
 
-	for _, item := range postmanCollection.Items {
-		fmt.Println(item.Name)
-		for _, subItem := range item.Items {
-			fmt.Println("  -[", subItem.Request.Method, "]", subItem.Name)
-		}
-	}
+	return postmanCollection
+}
 
-	exportToFile(postmanCollection)
+func flagModifier() {
+	if !strings.Contains(*baseUrlFlag, "http://") && !strings.Contains(*baseUrlFlag, "https://") {
+		baseUrl := PROTOCOL + "://" + *baseUrlFlag
+		*baseUrlFlag = baseUrl
+	}
 }
 
 func exportToFile(postmanCollection postman.Collection) {
